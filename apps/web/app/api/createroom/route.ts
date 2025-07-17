2ef709f510d86666c2455a0745f375c6d3759418
abuzerexe <abouzer381@gmail.com>
2025-07-17 22:37:42 +0500

    create room logic

--- apps/web/app/api/createroom/route.ts
+++ apps/web/app/api/createroom/route.ts
@@ -2,23 +2,47 @@
 import { NextRequest, NextResponse } from "next/server";
 import { currentUser } from '@clerk/nextjs/server'
 import {CreateRoomSchema} from "@workspace/common/types"
+import {prisma} from "@workspace/db/prisma"
 
+export async function POST(req: NextRequest){
+    try{    
+        const user = await currentUser()
+        
+        if(!user){
+            return NextResponse.json({
+                error: "Unauthroized"
+            }, { status: 401 })
+        }
+        
+        const {roomName} = await req.json()
+        const check = CreateRoomSchema.safeParse(roomName)
 
-export async function GET(req: NextRequest){
-    const user = await currentUser()
-      
-    if(!user){
+        if(!check.success){
+            return NextResponse.json({
+                error: check.error.message
+            }, { status: 422 })
+        }
+
+        const success = await prisma.room.create({
+            data:{
+                slug : roomName,
+                adminId: user.id
+            }
+        })
+
+        if(!success){
+            return NextResponse.json({
+                error: "Error"
+            }, { status: 500 })
+        }
+
         return NextResponse.json({
-            error: "Unauthroized"
-        }, { status: 401 })
+            msg: "Room created Successfully."
+        })
+        
+    }catch(e:any){
+        return NextResponse.json({
+            error: "Error."
+        }, { status: 500 })
     }
-
-    new WebSocket("ws://localhost:3002")
-    console.log("connected to ws")
-
-
-    return NextResponse.json({
-        error: "Reached"
-    })
-
 }
