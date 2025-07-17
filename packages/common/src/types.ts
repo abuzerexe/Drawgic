13e7c84f2c1458f266e6d76a990751f2286b8e33
abuzerexe <abouzer381@gmail.com>
2025-07-17 22:37:14 +0500

    added db

--- packages/common/src/types.ts
+++ packages/common/src/types.ts
@@ -1,3 +1,3 @@
 import {z} from "zod";
 
-export const CreateRoomSchema = z.string().min(3).max(30)
+export const CreateRoomSchema = z.string().min(3,{message:"Too Short."}).max(40,{message:"Too Long."})
